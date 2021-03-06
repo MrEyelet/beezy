deb413620d24f66d271ce89c22553236a29d2b3c
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-06 12:56:18 +0100

    init base

---
+++ src/main.js
@@ -0,0 +1,5 @@
+//Styles
+import "./scss/style.scss"
+
+//Js
+import "./js/scripts.js"
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const webpack = require("webpack")
+const path = require("path")
+
+module.exports = {
+  entry: "./src/main.js",
+  output: {
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, "./dist")
+  },
+  watch: false,
+  mode: "development",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/, //Regular expression
+        exclude: /(node_modules|bower_components)/, //excluded node_modules
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"] //Preset used for env setup
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["autoprefixer"]]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass") // Prefer `dart-sass`
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [["autoprefixer"]]
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
+}
