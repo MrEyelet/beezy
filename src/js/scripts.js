f891a8dce5c46726684c8b1d808680d8d529b36d
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-07 16:00:21 +0100

    feat: form css validate

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,5 +3,5 @@ import Test from "./modules/Test"
 import AppearElements from "./modules/AppearElements"
 
 // Instantiate a new object using our modules/classes
-// var test = new Test()
+var test = new Test()
 var appearElements = new AppearElements()
