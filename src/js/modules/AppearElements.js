1dc05cbc1a019319c4f015d053acc8a3da49e842
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-07 12:51:45 +0100

    feat: desktop pxp

--- src/js/modules/AppearElements.js
+++ src/js/modules/AppearElements.js
@@ -28,7 +28,7 @@ class AppearElements {
     this.tlShowElements.to(".cover + .reveal", { duration: 0, opacity: 1 }, "=-0.15")
     this.tlShowElements.to(".cover", { duration: 0.75, scaleY: 0, transformOrigin: "bottom", ease: Power4.easeOut })
     this.tlShowElements.from(".form__button", { duration: 0.75, opacity: 0, ease: Power4.easeOut }, "=-0.25")
-    this.tlShowElements.from(".form__input", { duration: 0.75, width: 0, paddingLeft: 0, ease: Power4.easeOut })
+    this.tlShowElements.to(".form__input", { duration: 0.75, width: "100%", paddingLeft: "2rem", ease: Power4.easeOut })
     this.tlShowElements.from(".form__label", { duration: 0.75, opacity: 0, ease: Power4.easeOut }, "=-0.75")
     this.tlShowElements.from(".socials__ic", { duration: 0.75, opacity: 0, x: "20px", stagger: 0.2, ease: Power4.easeOut }, "=-0.75")
     setTimeout(() => {
--- src/js/modules/Test.js
+++ src/js/modules/Test.js
@@ -1,6 +1,14 @@
 class Test {
   constructor() {
     console.log("test log")
+    this.real100vh()
+  }
+
+  real100vh() {
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty("--vh", `${vh}px`)
+    })
   }
 }
 
