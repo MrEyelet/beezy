feec7049869b99402c395dd0b1fa3b148095ab3a
Marcin <marcin.adam.mroczko@gmail.com>
2021-03-08 22:05:19 +0100

    bundle

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/main.js",
   output: {
     path: path.join(__dirname, "/dist"),
-    publicPath: "/dist",
+    publicPath: "/dist/",
     filename: "bundle.min.js"
   },
   watch: false,
